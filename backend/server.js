8b468969932a3b2a52f8a322fa33cb7ee211734e
ishtpreet <ishtpreet99@gmail.com>
2024-11-25 20:30:14 -0700

    Added simple random joke logic

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,35 @@
 const WebSocket = require('ws');
+const axios = require('axios');
 
-const server = new WebSocket.Server({ port: 8080 });
+const server = new WebSocket.Server({ port: 3001 });
+
+
+const CHUCK_NORRIS_API = 'https://api.chucknorris.io/jokes/random';
+
+const getChuckNorrisJoke = async () => {
+  const response = await axios.get(CHUCK_NORRIS_API);
+  return response.data.value;
+}
 
 server.on('connection', (socket) => {
   console.log('Client connected');
 
   socket.send('Welcome to the WebSocket server!');
+  setInterval(async () => {
+    const joke = await getChuckNorrisJoke();
+    socket.send(joke);
+  }
+  , 3500);
 
   socket.on('message', (message) => {
     console.log(`Received message: ${message}`);
 
     socket.send(`Server received: ${message}`);
+    
   });
 
   socket.on('close', () => {
     console.log('Client disconnected');
   });
 });
+
