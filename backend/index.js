437878924afd2d94ac6bf3b22a85d4b3fd2004fd
ishtpreet <ishtpreet99@gmail.com>
2024-12-05 10:43:41 -0700

    Fixed Backend Port

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,10 @@ const startWebRTCServer = require('./webrtcServer');
 
 const startServers = () => {
   // Start WebSocket server on port 3001
-  startWebSocketServer(3001);
+  startWebSocketServer(process.env.PORT || 3001);
 
   // Start WebRTC signaling server on port 3002
-  startWebRTCServer(3002);
+  startWebRTCServer(process.env.PORT || 3002);
 
   console.log('Both WebSocket and WebRTC servers are running');
 };
