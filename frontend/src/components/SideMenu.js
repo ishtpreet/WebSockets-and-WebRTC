14db5bac1aa29a36c828b25983b5533c3bb12dd2
ishtpreet <ishtpreet99@gmail.com>
2024-12-06 13:21:41 -0700

    Bitcoing Price trackign adn navbar

--- frontend/src/components/SideMenu.js
+++ frontend/src/components/SideMenu.js
@@ -8,149 +8,123 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
 import SensorsIcon from '@mui/icons-material/Sensors';
-// import ConnectWithoutContactIcon from '@mui/icons-material/ConnectWithoutContact';
 import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
-import {useNavigate} from 'react-router'
-import Typography from '@mui/material/Typography';
-
+import { useNavigate, useLocation} from 'react-router';
 
 const drawerWidth = 240;
 
-function ResponsiveDrawer(props) {
+function SideMenu(props) {
     const navigate = useNavigate();
+    const location = useLocation();
 
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-  const [isClosing, setIsClosing] = React.useState(false);
+    const { window } = props;
+    const [mobileOpen, setMobileOpen] = React.useState(false);
 
-  const handleDrawerClose = () => {
-    setIsClosing(true);
-    setMobileOpen(false);
-  };
+    const handleDrawerClose = () => {
+        setMobileOpen(false);
+    };
 
-  const handleDrawerTransitionEnd = () => {
-    setIsClosing(false);
-  };
-
-//   const handleDrawerToggle = () => {
-//     if (!isClosing) {
-//       setMobileOpen(!mobileOpen);
-//     }
-//   };
-
-const handleClick = (type ,e, index) => {
-    // console.log(index, e);
-    e.preventDefault();
-    if(type === 'ws'){
-    if(index === 0){
-        navigate('/');
+    const handleClick = (type, e, index) => {
+        e.preventDefault();
+        if (type === 'ws') {
+            if (index === 0) navigate('/');
+            if (index === 1) navigate('/what-is-ws');
+            if (index === 2) navigate('/ws');
+        } else if (type === 'webrtc') {
+            if (index === 0) navigate('/why-webrtc');
+            if (index === 1) navigate('/what-is-webrtc');
+            if (index === 2) navigate('/webrtc');
         }
-    if(index === 1){
-        navigate('/what-is-ws');
-        }
-    if(index === 2){
-        navigate('/ws');
-        }
-    // if(index === 3){
-    //     navigate('/ws-file-transfer');
-    //     }
-      }
-      else if(type === 'webrtc'){
-        if(index === 0){
-            navigate('/why-webrtc');
-            }
-        if(index === 1){
-            navigate('/what-is-webrtc');
-            }
-        if(index === 2){
-            navigate('/webrtc');
-            }
-          }
+    };
 
+    const drawer = (
+        <div>
+            <Toolbar>
+                <Typography variant="h6" noWrap component="div">
+                    SER-598 Project on
+                </Typography>
+            </Toolbar>
+            <Divider />
+            <List>
+                {['Why Websockets?', 'What is WS?', 'WS in Action'].map((text, index) => {
+                    const pathMap = ['/', '/what-is-ws', '/ws'];
+                    return (
+                        <ListItem key={text} disablePadding>
+                            <ListItemButton
+                                onClick={(e) => handleClick('ws', e, index)}
+                                selected={location.pathname === pathMap[index]} 
+                            >
+                                <ListItemIcon>
+                                    {(index === 0 || index === 1) && <QuestionMarkIcon />}
+                                    {index === 2 && <SensorsIcon />}
+                                </ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItemButton>
+                        </ListItem>
+                    );
+                })}
+            </List>
+            <Divider />
+            <List>
+                {['Why Web RTC?', 'What is Web RTC?', 'WebRTC in Action'].map((text, index) => {
+                    const pathMap = ['/why-webrtc', '/what-is-webrtc', '/webrtc'];
+                    return (
+                        <ListItem key={text} disablePadding>
+                            <ListItemButton
+                                onClick={(e) => handleClick('webrtc', e, index)}
+                                selected={location.pathname === pathMap[index]} 
+                            >
+                                <ListItemIcon>
+                                    {(index === 0 || index === 1) && <QuestionMarkIcon />}
+                                    {index === 2 && <OndemandVideoIcon />}
+                                </ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItemButton>
+                        </ListItem>
+                    );
+                })}
+            </List>
+        </div>
+    );
+
+    const container = window !== undefined ? () => window().document.body : undefined;
+
+    return (
+        <Box
+            component="nav"
+            sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+            aria-label="mailbox folders"
+        >
+            <Drawer
+                container={container}
+                variant="temporary"
+                open={mobileOpen}
+                onClose={handleDrawerClose}
+                ModalProps={{
+                    keepMounted: true,
+                }}
+                sx={{
+                    display: { xs: 'block', sm: 'none' },
+                    '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                }}
+            >
+                {drawer}
+            </Drawer>
+            <Drawer
+                variant="permanent"
+                sx={{
+                    display: { xs: 'none', sm: 'block' },
+                    '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                }}
+                open
+            >
+                {drawer}
+            </Drawer>
+        </Box>
+    );
 }
 
-  const drawer = (
-    <div>
-      <Toolbar>
-      <Typography variant="h6" noWrap component="div">
-            SER-598 Project on
-          </Typography>
-      </Toolbar>
-      <Divider />
-      <List>
-        {/* TODO: WS File Transfer */}
-        {['Why Websockets?', 'What is WS?', 'WS in Action'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-                
-            <ListItemButton onClick={(e)=> handleClick('ws', e, index)}>
-              <ListItemIcon>
-                {(index === 0 || index === 1) && <QuestionMarkIcon />}
-                {index === 2 && <SensorsIcon />}
-                {/* {index === 3 && <ConnectWithoutContactIcon />} */}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['Why Web RTC?', 'What is Web RTC?', 'WebRTC in Action'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton onClick={(e) => handleClick('webrtc', e, index)}>
-              <ListItemIcon>
-              {(index === 0 || index === 1) && <QuestionMarkIcon />}
-                {index === 2 && <OndemandVideoIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
-
-  // Remove this const when copying and pasting into your project.
-  const container = window !== undefined ? () => window().document.body : undefined;
-
-  return (
-      <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-        aria-label="mailbox folders"
-      >
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onTransitionEnd={handleDrawerTransitionEnd}
-          onClose={handleDrawerClose}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-        >
-          {drawer}
-        </Drawer>
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-          open
-        >
-          {drawer}
-        </Drawer>
-     
-    </Box>
-  );
-}
-
-export default ResponsiveDrawer;
+export default SideMenu;
--- frontend/src/pages/ws/WS.js
+++ frontend/src/pages/ws/WS.js
@@ -3,16 +3,36 @@ import Button from '@mui/material/Button';
 import ConnectWithoutContactIcon from '@mui/icons-material/ConnectWithoutContact';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
+import Paper from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import TabPanel from '../../components/TabPanel';
+import Typography from '@mui/material/Typography';
+import CurrencyBitcoinIcon from '@mui/icons-material/CurrencyBitcoin';
 
 export default function WS() {
 
     const [ws, setWs] = useState(null);
+    const [binanceWs, setBinanceWs] = useState(null);
+    const [binanceConnected, setBinanceConnected] = useState(false);
     const [msg, setMsg] = useState('');
     const [connected, setConnected] = useState(false);
     const [open, setOpen] = useState(false);
     const [data, setData] = useState('');
     const [alertType, setAlertType] = useState('success');
     const [duration, setDuration] = useState(5000);
+    const [value, setValue] = useState(0);
+    const [price, setPrice] = useState(0);
+    const [priceWS, setPriceWS] = useState(0);
+    const [showPrice, setShowPrice] = useState(false);
+    const [showPriceWS, setShowPriceWS] = useState(false);
+
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+      }
 
     const handleConnect = (e) => {
         e.preventDefault();
@@ -55,13 +75,126 @@ export default function WS() {
         setOpen(false);
       }
 
-      
+      const getBitcoinPrice = async () => {
+        setShowPrice(true);
+        const response = await fetch('https://data-api.binance.vision/api/v3/ticker/price?symbol=BTCUSDT');
+        const data = await response.json();
+        setPrice(data.price);
+      }
+
+      const getBitcoinPriceWS = async () => {
+        setShowPriceWS(true);
+        const ws = new WebSocket('wss://data-stream.binance.vision:443/ws/btcusdt@kline_1s');
+        setBinanceWs(ws);
+        ws.onopen = () => {
+          console.log('Binance WebSocket connected');
+          setMsg('Connected to Binance WebSocket');
+          setOpen(true);
+          setBinanceConnected(true);
+        };
+        ws.onmessage = async (message) => {
+          console.log('Received:', message.data);
+        //   setData(message.data);
+        const msg = await JSON.parse(message.data);
+        console.log('Price:', msg.k.c, msg.k);
+          setPriceWS(msg.k.c);
+          setDuration(2000);
+          setMsg('Received data from Binance WS server');
+          setAlertType('info');
+          setOpen(true);
+        };
+        }
+
+        const handleBinanceDisconnect = (e) => {
+            e.preventDefault();
+            if (binanceWs) {
+              binanceWs.close();
+            }
+            
+            setMsg('Disconnected from Binance WebSocket');
+            setDuration(5000)
+            setAlertType('warning');
+            setOpen(true);
+            setBinanceConnected(false);
+          }
+
 
     return (<>
-        <h1>WebSocket & WebRTC &nbsp; {connected && <ConnectWithoutContactIcon />}</h1>
-        <p>{data}</p>
+        <h1>WebSockets in Action &nbsp; {connected && <ConnectWithoutContactIcon />}</h1>
+        <h4>Step 1. Open Your Browser Dev Tools.</h4>
+        <h4>Step 2. Head over to Network Tab.</h4>
+        <h4>Step 3. (Optional) Clear the network requests.</h4>
+        <Divider />
+        <br />
+        <Tabs value={value} onChange={handleChange} centered>
+        <Tab label="Bitcoin Price" />
+        <Tab label="Random Chuck Norris Joke" />
+        </Tabs>
+        <TabPanel value={value} index={0}>
+        <Stack spacing={1}>
+            <Paper>
+            <Typography variant="h6" gutterBottom>Without Using WebSocket, the client would have to poll the server for updates.</Typography>                
+            </Paper>
+            {showPrice && <Paper sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                }}>
+                <h3>Bitcoin Price: ${price}</h3>
+            </Paper>}
+            <Paper 
+            sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                }}
+            >
+                <Button variant="contained" sx={{backgroundColor: '#FFB20F', color: '#000'}} onClick={getBitcoinPrice}><CurrencyBitcoinIcon />HTTP API</Button>
+            </Paper>
+            <Divider />
+            <Paper>
+            <Typography variant="h6" gutterBottom>Using WebSockets to communicate between the client and the server.</Typography>
+            </Paper>
+            {showPriceWS && <Paper sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                }}>
+                <h3>Bitcoin Price: ${priceWS}</h3>
+            </Paper>}
+            <Paper 
+            sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                }}
+            >
+                {/* FFEC51, F7931A */}
+               {!binanceConnected ? <Button variant="contained" sx={{backgroundColor: '#FFB20F', color: '#000'}} onClick={getBitcoinPriceWS}><CurrencyBitcoinIcon />WS</Button>
+               : <Button variant="contained" color="warning" onClick={handleBinanceDisconnect}>Disconnect</Button> 
+               }
+            </Paper>
+            
+        </Stack>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+        <Stack spacing={2} divider={<Divider orientation="horizontal" flexItem />}>
+            <Paper>
+                <Typography variant="h6" gutterBottom>Using WebSockets to communicate between the client and the server.</Typography>
+                <p>{data}</p>
         
-       {!connected ? <Button variant="contained" color="primary" onClick={handleConnect}>Connect to WS</Button> : <Button variant="contained" color="secondary" onClick={handleDisconnect}>Disconnect</Button>}
+        {!connected ? <Button variant="contained" color="primary" onClick={handleConnect}>Connect to WS</Button> : <Button variant="contained" color="secondary" onClick={handleDisconnect}>Disconnect</Button>}
+            </Paper>
+            <Paper>
+                <Typography variant="h6" gutterBottom>Without Using WebSocket, the client would have to poll the server for updates.</Typography>
+
+            </Paper>
+            
+        </Stack>
+        </TabPanel>
+
+        
+       
        <Snackbar open={open} autoHideDuration={duration} onClose={handleClose}>
           <Alert
             onClose={handleClose}
